00e7d0159aa239f7395a6ec2e19422045506e717
Jan Paul Posma <janpaul123@users.noreply.github.com>
2014-05-09 17:56:28 +0200

    Implemented first tests

--- test/challenges_iframe_api_spec.js
+++ test/challenges_iframe_api_spec.js
@@ -1,4 +1,23 @@
 describe(ChallengesIframeApi, function() {
-  it('needs tests', function() {
+  it('reads the challenges from the attributes', function(done) {
+    var challenges = [{prompt: 'Sky?', answers: 'blue', scoring: 'strict'}];
+
+    var iframeApi = new ChallengesIframeApi(function(options) {
+      chai.expect(options.challenges).to.deep.equal(challenges);
+      done();
+    });
+
+    window.postMessage({event: 'attributesChanged', data: {'vs-challenges': challenges}}, '*');
+  });
+
+  it('reads the scoring from the learnerState', function(done) {
+    var scoring = {responses: ['blue'], scores: [1], totalScore: 1};
+
+    var iframeApi = new ChallengesIframeApi(function(options) {
+      chai.expect(options.scoring).to.deep.equal(scoring);
+      done();
+    });
+
+    window.postMessage({event: 'learnerStateChanged', data: {'vs-scores': scoring}}, '*');
   });
 });
