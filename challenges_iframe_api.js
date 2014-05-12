3a84deeec2c6de3365984b29dd09bccb5317c869
Mike Bannister <notimpossiblemike@gmail.com>
2014-05-12 08:43:29 -0700

    wip

--- challenges_iframe_api.js
+++ challenges_iframe_api.js
@@ -1,74 +1,4 @@
-window.ChallengesIframeApi = function(changeCallback) {
-  this._handleMessage = this._handleMessage.bind(this);
-  window.addEventListener('message', this._handleMessage);
-  this._challenges = [];
-  this._scoring = {};
-  this._changeCallback = changeCallback;
-};
-
-window.ChallengesIframeApi.prototype.destroy = function() {
-  window.removeEventListener('message', this._handleMessage);
-};
-
-window.ChallengesIframeApi.prototype.setChallenges = function(challenges) {
-  if (!_.isArray(challenges)) throw new Error('challenges argument must be an array');
-
-  window.parent.postMessage({event: 'setAttributes', data: {'vs-challenges': challenges}}, '*');
-};
-
-window.ChallengesIframeApi.prototype.scoreChallenges = function(responses) {
-  if (!_.isArray(responses)) throw new Error('responses argument must be an array');
-  if (responses.length !== this._challenges.length) throw new Error('responses argument must have same length as challenges');
-
-  var scores = [];
-  var totalScore = 0;
-  for (var i=0; i<this._challenges.length; i++) {
-    scores[i] = this._scoreChallenge(this._challenges[i], responses[i]);
-    totalScore += scores[i];
-  }
-
-  var scoring = {responses: responses, scores: scores, totalScore: totalScore};
-  window.parent.postMessage({event: 'setLearnerState', data: {'vs-scores': scoring}}, '*');
-};
-
-window.ChallengesIframeApi.prototype._handleMessage = function(event) {
-  var eventName = event.data.event;
-  if(eventName) {
-    var handler = this._messageHandlers[eventName];
-    if(handler) {
-      handler.call(this, event.data.data);
-    }
-  }
-};
-
-window.ChallengesIframeApi.prototype._callChangeCallback = function() {
-  this._changeCallback({challenges: this._challenges, scoring: this._scoring});
-};
-
-window.ChallengesIframeApi.prototype._messageHandlers = {
-  attributesChanged: function(attributes) {
-    if (!_.isEqual(this._challenges, attributes['vs-challenges'])) {
-      this._challenges = attributes['vs-challenges'];
-      this._callChangeCallback();
-    }
-  },
-
-  learnerStateChanged: function(learnerState) {
-    if (!_.isEqual(this._scoring, learnerState['vs-scores'])) {
-      this._scoring = learnerState['vs-scores'];
-      this._callChangeCallback();
-    }
-  }
-};
-
-window.ChallengesIframeApi.prototype._scoreChallenge = function(challenge, response) {
-  var scorer = this._scoringStrategies[challenge.scoring];
-  if (!scorer) throw new Error('Unknown challenge scorer: ' + challenge.scoring);
-
-  return scorer(challenge.answers, response);
-};
-
-window.ChallengesIframeApi.prototype._scoringStrategies = {
+var strategies = {
   strict: function(prompt, response) {
     return _.isEqual(prompt, response) ? 1 : 0;
   },
@@ -109,3 +39,35 @@ window.ChallengesIframeApi.prototype._scoringStrategies = {
     return (start <= response && response <= end) ? 1 : 0;
   }
 };
+
+var calculate = function(strategy, answer, response) {
+  var scorer = strategies[strategy];
+  if (!scorer) throw new Error('Unknown scorer: ' + strategy);
+
+  return scorer(answer, response);
+};
+
+var scorer = function(strategy) {
+  return function(responses, answers) {
+
+    // TODO track me!!!
+    if (!_.isArray(responses)) throw new Error('responses argument must be an array');
+    if (responses.length !== answers.length) throw new Error('responses argument must have same length as answers');
+
+    var scores = [];
+    var totalScore = 0;
+    for (var i=0; i<answers.length; i++) {
+      scores[i] = calculate(strategy, answers[i], responses[i]);
+      totalScore += scores[i];
+    }
+
+    return {responses: responses, scores: scores, totalScore: totalScore};
+  }
+};
+
+window.ChallengesApi = {
+  strict: scorer('strict'),
+  partial: scorer('partial'),
+  subset: scorer('subset'),
+  range: scorer('range')
+}
--- test/challenges_iframe_api_spec.js
+++ test/challenges_iframe_api_spec.js
@@ -1,4 +1,16 @@
-describe(ChallengesIframeApi, function() {
-  it('needs tests', function() {
+describe(ChallengesApi, function() {
+  it('performs a strict match', function(done) {
+    dump(ChallengesApi);
+    var answers = ['blue'];
+    var responses = ['blue']
+    dump(ChallengesApi.strict(answers, responses));
+    done();
+    
+    // var iframeApi = new ChallengesIframeApi(function(options) {
+    //   chai.expect(options.challenges).to.deep.equal(challenges);
+    //   done();
+    // });
+    //
+    // window.postMessage({event: 'attributesChanged', data: {'vs-challenges': challenges}}, '*');
   });
 });
